5148c852c13d1a868447ac690ff55bd0530e0d8f
Ilya <fybiby.2@yandex.ru>
2024-08-21 14:56:00 +0300

    Add Item

--- src/main/java/ru/practicum/shareit/item/model/Item.java
+++ src/main/java/ru/practicum/shareit/item/model/Item.java
@@ -1,7 +1,17 @@
 package ru.practicum.shareit.item.model;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
 /**
  * TODO Sprint add-controllers.
  */
+@Data
+@EqualsAndHashCode(of = "id")
 public class Item {
+    long id;
+    long ownerId;
+    String name;
+    String description;
+    boolean available;
 }
