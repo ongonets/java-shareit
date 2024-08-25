2abf0c3b085d8a778c55a2285e7116c39e14289d
Ilya <fybiby.2@yandex.ru>
2024-08-25 12:35:45 +0300

    Add booking

--- src/main/java/ru/practicum/shareit/booking/Booking.java
+++ src/main/java/ru/practicum/shareit/booking/Booking.java
@@ -1,7 +1,20 @@
 package ru.practicum.shareit.booking;
 
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+
 /**
  * TODO Sprint add-bookings.
  */
+@Data
+@EqualsAndHashCode(of = "id")
 public class Booking {
+    long id;
+    LocalDateTime start;
+    LocalDateTime end;
+    long itemId;
+    long bookerId;
+    BookingStatus status;
 }
