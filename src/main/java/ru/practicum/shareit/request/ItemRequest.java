96ac558d8f94c59dff34fc8599d13a6c488d4f63
Ilya <fybiby.2@yandex.ru>
2024-08-25 12:39:59 +0300

    Add ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -3,5 +3,17 @@ package ru.practicum.shareit.request;
 /**
  * TODO Sprint add-item-requests.
  */
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+
+@Data
+@EqualsAndHashCode(of = "id")
 public class ItemRequest {
+    long id;
+    long requesterId;
+    String description;
+    LocalDateTime created;
 }
