2b8ecab51e5022491acc3cd924d405ee2eb36192
Ilya <fybiby.2@yandex.ru>
2024-09-10 15:37:21 +0300

    Refactor user with JPA

--- src/main/java/ru/practicum/shareit/user/model/User.java
+++ src/main/java/ru/practicum/shareit/user/model/User.java
@@ -1,18 +1,38 @@
 package ru.practicum.shareit.user.model;
 
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.experimental.FieldDefaults;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.Objects;
 
 /**
  * TODO Sprint add-controllers.
  */
-@Data
-@EqualsAndHashCode(of = "id")
-@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@ToString
 public class User {
-    long id;
-    String email;
-    String name;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    private String email;
+
+    private String name;
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        User user = (User) object;
+        return id == user.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
