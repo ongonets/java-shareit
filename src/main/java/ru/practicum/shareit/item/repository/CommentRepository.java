7cba8f18e10fe72aa6da954da28ade783342ef00
Ilya <fybiby.2@yandex.ru>
2024-09-21 13:23:45 +0300

    Refactor CommentRepository

--- src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
+++ src/main/java/ru/practicum/shareit/item/repository/CommentRepository.java
@@ -8,5 +8,5 @@ import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment,Long> {
 
-    public List<Comment> findAllByItem(Item item);
+    List<Comment> findAllByItem(Item item);
 }
