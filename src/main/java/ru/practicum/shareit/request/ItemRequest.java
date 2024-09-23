721686bb473f16ee84e03b7bf193d9d5035c6a03
Ilya <fybiby.2@yandex.ru>
2024-09-23 14:35:10 +0300

    Refactor ItemRequest

--- src/main/java/ru/practicum/shareit/request/ItemRequest.java
+++ src/main/java/ru/practicum/shareit/request/ItemRequest.java
@@ -4,19 +4,47 @@ package ru.practicum.shareit.request;
  * TODO Sprint add-item-requests.
  */
 
+import jakarta.persistence.*;
 import lombok.AccessLevel;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 import lombok.experimental.FieldDefaults;
+import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
+import java.util.Objects;
 
-@Data
-@EqualsAndHashCode(of = "id")
+@Entity
+@Table(name = "requests")
 @FieldDefaults(level = AccessLevel.PRIVATE)
+@Getter
+@Setter
+@ToString
 public class ItemRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     long id;
-    long requesterId;
+
+    @ManyToOne
+    @JoinColumn(name = "requester_id")
+    User requester;
+
     String description;
+
     LocalDateTime created;
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+        ItemRequest that = (ItemRequest) object;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
